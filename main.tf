7c77c8183118325899bee7991ab1722f0b79473e
format27 <83480842+sgrady27@users.noreply.github.com>
2024-05-14 15:54:22 -0500

    Task 2

--- main.tf
+++ main.tf
@@ -1,73 +1,61 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.28.0"
+# Create a publically accessible web page with Terraform.  You must complete the following
+# 1) Terraform Script with a VPC
+# 2) Terraform script must have a VM within your VPC.
+# 3) The VM must have the homepage on it.
+# 4) The VM must have an publically accessible link to it.
+# 5) You must Git Push your script to your Github.
+# 6) Outpub file must show 1) Public IP, 2) VPC, 3) Subnet of the VM, 4) Internal IP of the VM.
+
+
+# add compute instance to the VPC
+ resource "google_compute_instance" "task2" {
+    name         = var.instance_name
+    machine_type = var.machine_type
+    zone         = var.zone
+
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+      size  = 10
+    }
+    # true by default
+    auto_delete = true
+  }
+
+  network_interface {
+    network    = var.network_name
+    subnetwork = var.subnet_name
+
+    access_config {
+      // Ephemeral public IP
     }
   }
+
+  tags = ["http-server"]
+
+  # metadata = {
+  #   startup-script-url = "${file("startup.sh")}"
+  # }
+
+  metadata_startup_script = file("startup.sh")
+
+  # metadata_startup_script = <<-EOF
+  #   #!/bin/bash
+  #   apt-get update
+  #   apt-get install -y apache2
+  #   cat <<EOT > /var/www/html/index.html
+  #   <html>
+  #     <head>
+  #       <title>Welcome to My Homepage</title>
+  #     </head>
+  #     <body>
+  #       <h1>Welcome to My Homepage!</h1>
+  #       <p>This page is served by Apache on a Google Compute Engine VM instance.</p>
+  #     </body>
+  #   </html>
+  #   EOT
+  # EOF
 }
 
-provider "google" {
-  # Configuration options
-  project      = "vocal-antler-416401"
-  region       = "us-central1"
-  zone         = "us-central-a"
-  credentials  = "vocal-antler-416401-43589634bd2b.json"
-}
-
-resource "google_storage_bucket" "terraformguybucket1" {
-  name          = "wecanterraform"
-  location      = "EU"
-  force_destroy = true
-
-  uniform_bucket_level_access = false
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-}
-
-# Bucket ACL to public read
-resource "google_storage_bucket_acl" "bucket_acl" {
-  bucket         = google_storage_bucket.terraformguybucket1.name
-  predefined_acl = "publicRead"
-}
-
-# Uploading and setting public read access for HTML files
-resource "google_storage_bucket_object" "upload_html1" {
-  for_each     = fileset("${path.module}/", "*.html")
-  bucket       = google_storage_bucket.terraformguybucket1.name
-  name         = each.value
-  source       = "${path.module}/${each.value}"
-  content_type = "text/html"
-}
-
-# Public ACL for each HTML file
-resource "google_storage_bucket_acl" "html_acl1" {
-  for_each       = google_storage_bucket_object.upload_html1
-  bucket         = google_storage_bucket_object.upload_html1[each.key].bucket
-  #object         = google_storage_bucket_object.upload_html1[each.key].name
-  predefined_acl = "publicRead"
-}
-
-# Uploading and setting public read access for image files
-resource "google_storage_bucket_object" "upload_image1" {
-  for_each     = fileset("${path.module}/", "*.jpg")
-  bucket       = google_storage_bucket.terraformguybucket1.name
-  name         = each.value
-  source       = "${path.module}/${each.value}"
-  content_type = "image/jpeg"
-}
-
-# Public ACL for each image file
-resource "google_storage_bucket_acl" "image_acl1" {
-  for_each       = google_storage_bucket_object.upload_image1
-  bucket         = google_storage_bucket_object.upload_image1[each.key].bucket
-  #object         = google_storage_bucket_object.upload_image1[each.key].name
-  predefined_acl = "publicRead"
-}
-
-output "website_url" {
-  value = "https://storage.googleapis.com/${google_storage_bucket.terraformguybucket1.name}/index.html"
-}
